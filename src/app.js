d1c7dd876941c969f64e55af0d4b0641d9f9514b
Rathnayake M.R.T.N <99705847+IT21253294tharindu@users.noreply.github.com>
2025-03-12 15:10:10 +0530

    feedback component route

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import BASE_URL_V1 from "./config/base-url.js";
 import authRoutes from "./routes/auth.routes.js";
 import claimRoutes from "./routes/claim.routes.js";
 import AuthGuard from "./middlewares/auth-guard.js";
-
+import feedbackRoutes from "./routes/feedback.routes.js";
+import reportRoutes from "./routes/report.routes.js";
 //! Initialize Express app
 const app = express();
 
@@ -21,6 +22,8 @@ app.get("/", (req, res) => {
 app.use(AuthGuard); // Auth Middleware
 app.use(`${BASE_URL_V1}/auth`, authRoutes);
 app.use(`${BASE_URL_V1}/claims`, claimRoutes);
+app.use(`${BASE_URL_V1}/feedback`, feedbackRoutes);
+/* app.use(`${BASE_URL_V1}/report`, reportRoutes); */
 // app.use(`${BASE_URL_V1}/role`, roleRoutes);
 
 export default app;
